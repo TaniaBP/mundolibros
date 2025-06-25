22d74325c8a8f210355954cd43fdc2fdf99a75a1
TaniaBP <153878664+TaniaBP@users.noreply.github.com>
2025-06-25 05:07:54 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+
+</head>
+<body>
+    <?php require_once('php/dis/pg-inc.php'); ?>
+
+  </body>
+</body>
+</html>
